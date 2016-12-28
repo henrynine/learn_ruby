eadec0a3283579589b87423a66c7b589a2c6fedb
Henry Middleton <h78900@gmail.com>
2016-12-28 11:30:03 -0500

    05 done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+  attr_accessor :title, :little_words
+
+  def initialize
+    @little_words = ['a', 'an', 'the', 'at', 'by', 'for', 'in', 'of', 'on', 'to', 'up', 'and', 'as', 'but', 'or', 'nor', 'over']
+
+  def title=(t)
+    words = t.split(' ')
+    words.length.times do |i|
+      w = words[i]
+      unless @little_words.include? w
+        words[i] = w.capitalize
+      end
+    end
+    words[0].capitalize!
+    @title = words.reduce(''){|t, w| t + w + ' '}[0..-2]
+  end
+end
 end
