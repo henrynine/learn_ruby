e5df0286a0aae65a9209df780ff082294ca41288
Henry Middleton <h78900@gmail.com>
2016-12-27 23:23:57 -0500

    03 done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(word)
+  word
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, times = 2)
+  ((word + ' ') * times)[0..-2]
+end
+
+def start_of_word(word, amount)
+  word[0...amount]
+end
+
+def first_word(word)
+  word.split(' ').first
+end
+
+def titleize(word)
+  little_words = ['a', 'an', 'the', 'at', 'by', 'for', 'in', 'of', 'on', 'to', 'up', 'and', 'as', 'but', 'or', 'nor', 'over']
+  words = word.split(' ')
+  words.length.times do |i|
+    w = words[i]
+    unless little_words.include? w
+      words[i] = w.capitalize
+    end
+  end
+  words[0].capitalize!
+  words.reduce(''){|t, w| t + w + ' '}[0..-2]
+end
